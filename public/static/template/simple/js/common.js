cea461008e03edb34c6e67b271524c3af984038d
AcmenXiaoKe <kejincaizuishuai@gmail.com>
2021-06-03 11:04:12 +0800

    update view theme

--- public/static/template/simple/js/common.js
+++ public/static/template/simple/js/common.js
@@ -80,3 +80,43 @@ MobileNavMask.addEventListener('click', function () {
     MobileNav.style.left = '-70%';
     MobileNavMask.style.display = 'none';
 })
+
+
+var theme = document.querySelector('.theme')
+var theme_icon = theme.querySelector('.iconfont')
+var theme_type = localStorage.getItem("theme_type") || 'default';
+// 调用主题切换函数，传递一个当前处于那种主题
+SubjectSwitch(theme_type)
+theme.addEventListener('click', function () {
+    // 主题切换
+    if (theme_type == 'default') {
+        theme_type = 'darkly';
+        SubjectSwitch("darkly")
+    } else {
+        SubjectSwitch("default")
+        theme_type = 'default';
+    }
+})
+
+
+function SubjectSwitch(val) {
+    if (val == "darkly") {
+        localStorage.setItem("theme_type", "darkly")
+        document.documentElement.style.setProperty('--html-bg', "#121212")
+        document.documentElement.style.setProperty('--Primary-bg', "#232323")
+        document.documentElement.style.setProperty('--title-color', "#F2F6FC")
+        document.documentElement.style.setProperty('--border-color', "rgba(255,255,255,.1)")
+        document.documentElement.style.setProperty('--color', "rgba(255,255,255,.8)")
+
+    }
+    else {
+        // theme_icon
+        localStorage.setItem("theme_type", "default")
+        document.documentElement.style.setProperty('--html-bg', "#F5F5F5")
+        document.documentElement.style.setProperty('--Primary-bg', "#FFF")
+        document.documentElement.style.setProperty('--title-color', "#363636")
+        document.documentElement.style.setProperty('--border-color', "#e4e7ed")
+        document.documentElement.style.setProperty('--color', "rgb(51,51,51)")
+    }
+
+}
